9a5f40da47dd178ca390445de7c70f03ffc1e74b
diixo <anatolii.podoba@gmail.com>
2024-06-04 15:41:33 +0300

    correct implementation of hash (fnv): uint8_t.

--- src/dictionary.cc
+++ src/dictionary.cc
@@ -163,7 +163,7 @@ std::string Dictionary::getWord(int32_t id) const {
 uint32_t Dictionary::hash(const std::string& str) const {
   uint32_t h = 2166136261;
   for (size_t i = 0; i < str.size(); i++) {
-    h = h ^ uint32_t(int8_t(str[i]));
+    h = h ^ uint32_t(uint8_t(str[i]));
     h = h * 16777619;
   }
   return h;
